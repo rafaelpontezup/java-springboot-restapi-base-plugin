4b0c3bf47996b170e6df560d76bcc34bdc2922aa
Rafael Ponte <rafael.ponte@zup.com.br>
2023-07-26 14:27:30 -0300

    adding template files and inputs in plugin.yaml;

---
+++ templates/src/test/java/{{computed_inputs.project_folder}}/{{project_name_capitalized}}ApplicationTests.java
@@ -0,0 +1,15 @@
+package {{computed_inputs.project_base_package}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class {{project_name_capitalized}}ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
